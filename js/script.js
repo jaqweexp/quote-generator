1db4c912d142746b5b859a89785cba1145818020
jaqweexp <jacquie.vchang@gmail.com>
2020-01-22 09:58:11 -0800

    Added project template files

---
+++ js/script.js
@@ -0,0 +1,77 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+
+var quotes [
+	{
+		quote: "Believe you can and you're halfway there.",
+		source: "Theodore Roosevelt"
+
+	},
+	{
+		quote: "To live is the rarest thing in the world. Most people exist, that is all",
+		source: "Oscar Wilde"
+	},
+	{
+		quote: "Live as if you were to die tomorrow. Learn as if you were to live forever",
+		source: "Mahatma Gandhi"
+	},
+	{
+		quote: "Without music, life would be a mistake.",
+		source: "Friedrich Nietzsche"
+	},
+	{
+		quote: "Life is what happens to us while we are making other plans.",
+		source: "Allen Saunders"
+	},
+	{
+		quote: "I have not failed. I've just found 10,000 ways that won't work.".
+		source: "Thomas A. Edison"
+	},
+	{
+		quote: "I may not have gone where I intended to go, but I think I have ended up where I needed to be.",
+		source: "Douglas Adams"
+	},
+	{
+		quote: "Outside of a dog, a book is man's best friend. Inside of a dog it's too dark to read.",
+		source: "Groucho Marx"
+	},
+	{
+		quote: "I am enough of an artist to draw freely upon my imagination. Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world."
+		source: "Albert Einstein"
+	},
+	{
+		quote: "It is never too late to be what you might have been.",
+		source: "George Eliot"
+	}
+
+]
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
