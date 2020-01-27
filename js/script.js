d45f9dc047c1c5eac13452f09740369e7af12459
jaqweexp <jacquie.vchang@gmail.com>
2020-01-27 10:41:48 -0800

    Get rid of undefined if source and or citation are not available. Added change background color function.

--- js/script.js
+++ js/script.js
@@ -11,10 +11,11 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-var quotes [
+var quotes = [
 	{
 		quote: "Believe you can and you're halfway there.",
-		source: "Theodore Roosevelt"
+		source: "Theodore Roosevelt",
+		year: 1903
 
 	},
 	{
@@ -27,14 +28,16 @@ var quotes [
 	},
 	{
 		quote: "Without music, life would be a mistake.",
-		source: "Friedrich Nietzsche"
+		source: "Friedrich Nietzsche",
+		citation: "Twilight of the Idols",
+		year: 1932
 	},
 	{
 		quote: "Life is what happens to us while we are making other plans.",
 		source: "Allen Saunders"
 	},
 	{
-		quote: "I have not failed. I've just found 10,000 ways that won't work.".
+		quote: "I have not failed. I've just found 10,000 ways that won't work.",
 		source: "Thomas A. Edison"
 	},
 	{
@@ -43,11 +46,13 @@ var quotes [
 	},
 	{
 		quote: "Outside of a dog, a book is man's best friend. Inside of a dog it's too dark to read.",
-		source: "Groucho Marx"
+		source: "Groucho Marx",
+		year: 1952
 	},
 	{
-		quote: "I am enough of an artist to draw freely upon my imagination. Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world."
-		source: "Albert Einstein"
+		quote: "I am enough of an artist to draw freely upon my imagination. Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
+		source: "Albert Einstein",
+		citation: "In his Lab"
 	},
 	{
 		quote: "It is never too late to be what you might have been.",
@@ -56,16 +61,66 @@ var quotes [
 
 ]
 
+//change background color
+
+var R;
+var G;
+var B;
+var newRGB;
+
+function changeBackground(){
+	R = Math.floor(Math.random() * 299);
+	G = Math.floor(Math.random() * 299);
+	B = Math.floor(Math.random() * 299);
+	newRGB = R + ',' + G + ',' + B;
+	document.body.style.background = 'rgb(' + newRGB + ')';
+}
 
 /***
  * `getRandomQuote` function
 ***/
+//get a random number from length of quotes
+//
 
+
+var randomNum;
+var chosenQuote;
+
+function getRandomQuote (){
+	randomNum = Math.floor(Math.random()*quotes.length);
+	chosenQuote = quotes[randomNum];
+}
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote (){
+
+	getRandomQuote();
+	quoteAndsourcehtml = '<p class="quote">' + chosenQuote.quote + '</p><p class="source">' + chosenQuote.source;
+
+	if (chosenQuote.citation == null && chosenQuote.year == null){
+		document.getElementById('quote-box').innerHTML = 
+		quoteAndsourcehtml +  '</p>';
+
+	}else if (chosenQuote.citation == null && chosenQuote.year !== null ){
+		document.getElementById('quote-box').innerHTML = 
+		quoteAndsourcehtml + '<span class="year">' + chosenQuote.year + '</span></p>';
+
+	}else if (chosenQuote.citation !== null && chosenQuote.year == null){
+		document.getElementById('quote-box').innerHTML = 
+		quoteAndsourcehtml + '<span class="citation">' + chosenQuote.citation + '</span></p>';
+
+	}else{
+	document.getElementById('quote-box').innerHTML = 
+	quoteAndsourcehtml + '<span class="citation">' + chosenQuote.citation + '</span><span class="year">' + chosenQuote.year + '</span></p>';
+	}	
+
+	changeBackground();
+
+} //close function
+	
 
 
 
